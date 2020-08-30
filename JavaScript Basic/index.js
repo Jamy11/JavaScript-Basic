988fa7321679ddb50fb698fbfaf450818c753d73
jamy11 <jamyarrahman82@gmail.com>
2020-08-30 11:39:12 +0600

    func objet added

--- JavaScript Basic/index.js
+++ JavaScript Basic/index.js
@@ -1,1 +1,38 @@
-console.log('hello world');
+let interestRate = 0.3;
+interestRate =1;
+console.log(interestRate);
+
+//primitive data types
+
+let name = 'jamy';
+let age = 30;
+let isApproved = true;
+let firstName = undefined;
+let selectedColor = null;
+
+//object 
+
+let persone = {
+    name : 'jamy',
+    age :30
+};
+
+//dot methode
+
+persone.name = 'foysal';
+
+//bracket methode 
+
+let selection = 'name';
+
+persone[selection] = 'mary';
+
+//console.log(persone.name)
+
+//function
+
+function greet(namee) {
+    console.log(namee)
+}
+
+greet('jamy')
